c23828717870f26c5abbc8ca9ea89ec1f560cc9a
Seth Battis <seth@battis.net>
2025-02-08 23:08:15 -0500

    chore(qui-cli.progress): restore issue

--- packages/progress/src/index.ts
+++ packages/progress/src/index.ts
@@ -2,6 +2,8 @@ import colors from '@battis/qui-cli.colors';
 import chalk from 'chalk';
 import cliProgress from 'cli-progress';
 
+// TODO convert progress to a full-fledged plugin
+
 type Options = {
   value?: number;
   max: number;
