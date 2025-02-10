d29ae63c9d4df6c67843bd7e4a77ba30a8cd6d96
Seth Battis <seth@battis.net>
2025-02-09 21:25:12 -0500

    feat(qui-cli.log)!: rewrite as Plugin.Container module
    BREAKING: No longer compatible with previous Plugin.Base class implementation.

--- packages/log/src/index.ts
+++ packages/log/src/index.ts
@@ -1,6 +1,7 @@
-import colors from '@battis/qui-cli.colors';
-import * as plugin from '@battis/qui-cli.plugin';
-import appRoot from '@battis/qui-cli.root';
+import { Colors } from '@battis/qui-cli.colors';
+import Plugin from '@battis/qui-cli.plugin';
+import { Root } from '@battis/qui-cli.root';
+import fs from 'node:fs';
 import ora from 'ora';
 import path from 'path';
 import stripAnsi from 'strip-ansi';
@@ -9,7 +10,7 @@ import { CustomLevels, DefaultLevels } from './Levels.js';
 
 export { CustomLevels, DefaultLevels };
 
-export type Options = {
+export type Configuration = Plugin.Configuration & {
   logFilePath?: string;
   stdoutLevel?: string;
   fileLevel?: string;
@@ -17,166 +18,141 @@ export type Options = {
   root?: string;
 };
 
-export class Log extends plugin.Base {
-  public static readonly defaults = {
-    logFilePath: undefined,
-    stdoutLevel: 'info',
-    fileLevel: 'all',
-    levels: DefaultLevels,
-    root: undefined
-  };
+let logFilePath: string | undefined = undefined;
+let stdoutLevel = 'info';
+let fileLevel = 'all';
+let levels: CustomLevels = DefaultLevels;
+let root: string | undefined = undefined;
 
-  private logger = winston.createLogger({
-    transports: []
-  });
+let logger = winston.createLogger({
+  transports: []
+});
+const transports: Record<string, winston.transport> = {
+  console: new winston.transports.Console({
+    format: winston.format.printf(({ message }) => message as string),
+    level: 'info'
+  })
+};
 
-  private transports: Record<string, winston.transport> = {
-    console: new winston.transports.Console({
-      format: winston.format.printf(({ message }) => message as string),
-      level: 'info'
-    })
-  };
-
-  private format = {
-    stripColors: winston.format((info) => {
-      for (const prop in info) {
-        if (typeof info[prop] === 'string') {
-          info[prop] = stripAnsi(info![prop]);
-        }
+const format = {
+  stripColors: winston.format((info) => {
+    for (const prop in info) {
+      if (typeof info[prop] === 'string') {
+        info[prop] = stripAnsi(info[prop] as string);
       }
-      return info;
-    })
-  };
-
-  private levels: CustomLevels = Log.defaults.levels;
-  private logFilePath: string | undefined = Log.defaults.logFilePath;
-  private stdoutLevel: string = Log.defaults.stdoutLevel;
-  private fileLevel: string = Log.defaults.fileLevel;
-  private root: string | undefined = Log.defaults.root;
-
-  private static singleton: Log | undefined = undefined;
-
-  public static getInstance(options: Options = {}) {
-    if (!this.singleton) {
-      this.singleton = new Log(options);
     }
-    this.singleton.reset(options);
-    return this.singleton;
-  }
+    return info;
+  })
+};
 
-  public constructor(options: Options = {}) {
-    super('log');
-    if (Log.singleton) {
-      throw new Error('Log is a singleton');
-    } else {
-      Log.singleton = this;
-    }
-    this.reset(options);
-  }
+const { name, dependencies } = await Plugin.define({
+  pathToPluginSourceDirectory: import.meta.dirname
+});
 
-  private reset({
-    logFilePath = Log.defaults.logFilePath,
-    stdoutLevel = Log.defaults.stdoutLevel,
-    fileLevel = Log.defaults.fileLevel,
-    levels = Log.defaults.levels,
-    root = Log.defaults.root
-  }: Options) {
-    this.logFilePath = logFilePath;
-    this.stdoutLevel = stdoutLevel;
-    this.fileLevel = fileLevel;
-    this.levels = levels;
-    this.root = root;
-  }
-
-  public options() {
-    return {
-      opt: {
-        logFilePath: {
-          description: `Path to log file (optional)`
-        },
-        stdoutLevel: {
-          description: `Log level to console stdout: ${colors.quotedValue('"off"')}, ${colors.quotedValue('"fatal"')}, ${colors.quotedValue('"error"')}, ${colors.quotedValue('"warning"')}, ${colors.quotedValue('"info"')}, ${colors.quotedValue('"debug"')}, ${colors.quotedValue('"trace"')}, or ${colors.quotedValue('"all"')} (default: ${colors.quotedValue(`"${Log.defaults.stdoutLevel}"`)})`,
-          default: Log.defaults.stdoutLevel
-        },
-        fileLevel: {
-          description: `Log level to log file (if path proved): ${colors.quotedValue('"off"')}, ${colors.quotedValue('"fatal"')}, ${colors.quotedValue('"error"')}, ${colors.quotedValue('"warning"')}, ${colors.quotedValue('"info"')}, ${colors.quotedValue('"debug"')}, ${colors.quotedValue('"trace"')}, or ${colors.quotedValue('"all"')} (default: ${colors.quotedValue(`"${Log.defaults.fileLevel}"`)})`,
-          default: Log.defaults.fileLevel
-        }
-      }
-    };
-  }
-
-  public init({
-    values: {
-      logFilePath = this.logFilePath,
-      stdoutLevel = this.stdoutLevel,
-      fileLevel = this.fileLevel
-    } = {}
-  }: plugin.Arguments<ReturnType<Log['options']>>) {
-    this.transports.console.level = stdoutLevel;
-    this.logger.configure({
-      levels: this.levels.levels,
-      transports: [this.transports.console]
-    });
-    if (logFilePath) {
-      const filename = path.resolve(this.root || appRoot(), logFilePath);
-      const spinner = ora(`Connecting to ${colors.url(filename)}`).start();
-      this.transports.file = new winston.transports.File({
-        filename,
-        level: fileLevel,
-        format: winston.format.combine(
-          this.format.stripColors(),
-          winston.format.timestamp(),
-          winston.format.json()
-        )
-      });
-      this.logger.add(this.transports.file);
-      winston.addColors(this.levels.colors);
-      spinner.succeed(
-        `Logging level ${colors.value(fileLevel)} to ${colors.url(filename)}`
-      );
-    }
-  }
-
-  public get() {
-    return this.logger || this.init({ positionals: [], values: {} }); // FIXME janky literal
-  }
-
-  private colorObject(obj: object) {
-    return JSON.stringify(obj, null, 2)
-      .replace(/: ([^"{[][^,\n]*)/g, `: ${colors.value('$1')}`)
-      .replace(/: ("([^"]|\\")*")/g, `: ${colors.quotedValue('$1')}`);
-  }
-
-  private namedLogMethod(level: string) {
-    return (message: string | object, ...meta: any[]) => {
-      if (typeof message != 'string') {
-        if (meta.some((elt: any) => elt.color === false)) {
-          if (message === undefined) {
-            message = 'undefined';
-          } else {
-            message = JSON.stringify(message, null, 2);
-          }
-        } else {
-          if (message === undefined) {
-            message = colors.value('undefined');
-          } else {
-            message = this.colorObject(message);
-          }
-        }
-      }
-      return this.logger.log(level, message, ...meta);
-    };
-  }
-
-  public log = this.logger.log.bind(this.logger);
-  public trace = this.namedLogMethod('trace');
-  public debug = this.namedLogMethod('debug');
-  public info = this.namedLogMethod('info');
-  public warning = this.namedLogMethod('warning');
-  public error = this.namedLogMethod('error');
-  public fatal = this.namedLogMethod('fatal');
+async function configure(config?: Configuration) {
+  logFilePath = config?.logFilePath;
+  stdoutLevel =
+    config?.stdoutLevel !== undefined ? config.stdoutLevel : stdoutLevel;
+  fileLevel = config?.fileLevel !== undefined ? config.fileLevel : fileLevel;
+  levels = config?.levels !== undefined ? config.levels : levels;
+  root = config?.root;
 }
 
-const log = new Log();
-export default log;
+function options() {
+  return {
+    opt: {
+      logFilePath: {
+        description: `Path to log file (optional)`
+      },
+      stdoutLevel: {
+        description: `Log level to console stdout: ${Colors.quotedValue('"off"')}, ${Colors.quotedValue('"fatal"')}, ${Colors.quotedValue('"error"')}, ${Colors.quotedValue('"warning"')}, ${Colors.quotedValue('"info"')}, ${Colors.quotedValue('"debug"')}, ${Colors.quotedValue('"trace"')}, or ${Colors.quotedValue('"all"')} (default: ${Colors.quotedValue(`"${stdoutLevel}"`)})`,
+        default: stdoutLevel
+      },
+      fileLevel: {
+        description: `Log level to log file (if path proved): ${Colors.quotedValue('"off"')}, ${Colors.quotedValue('"fatal"')}, ${Colors.quotedValue('"error"')}, ${Colors.quotedValue('"warning"')}, ${Colors.quotedValue('"info"')}, ${Colors.quotedValue('"debug"')}, ${Colors.quotedValue('"trace"')}, or ${Colors.quotedValue('"all"')} (default: ${Colors.quotedValue(`"${fileLevel}"`)})`,
+        default: fileLevel
+      }
+    }
+  };
+}
+
+function init({
+  values: {
+    logFilePath: filePath = logFilePath,
+    stdoutLevel: stdout = stdoutLevel,
+    fileLevel: file = fileLevel
+  } = {}
+}: Plugin.Arguments<ReturnType<typeof options>>) {
+  transports.console.level = stdout;
+  logger.configure({
+    levels: levels.levels,
+    transports: [transports.console]
+  });
+  if (filePath) {
+    const filename = path.resolve(root || Root.path(), filePath);
+    const spinner = ora(`Connecting to ${Colors.url(filename)}`).start();
+    transports.file = new winston.transports.File({
+      filename,
+      level: file,
+      format: winston.format.combine(
+        format.stripColors(),
+        winston.format.timestamp(),
+        winston.format.json()
+      )
+    });
+    logger.add(transports.file);
+    winston.addColors(levels.colors);
+    spinner.succeed(
+      `Logging level ${Colors.value(fileLevel)} to ${Colors.url(filename)}`
+    );
+  }
+}
+
+function get() {
+  return logger || init({ positionals: [], values: {} }); // FIXME janky literal
+}
+
+function colorObject(obj: object) {
+  return JSON.stringify(obj, null, 2)
+    .replace(/: ([^"{[][^,\n]*)/g, `: ${Colors.value('$1')}`)
+    .replace(/: ("([^"]|\\")*")/g, `: ${Colors.quotedValue('$1')}`);
+}
+
+function namedLogMethod(level: string) {
+  return (message: string | object, ...meta: any[]) => {
+    if (typeof message != 'string') {
+      if (meta.some((elt: any) => elt.color === false)) {
+        if (message === undefined) {
+          message = 'undefined';
+        } else {
+          message = JSON.stringify(message, null, 2);
+        }
+      } else {
+        if (message === undefined) {
+          message = Colors.value('undefined');
+        } else {
+          message = colorObject(message);
+        }
+      }
+    }
+    return logger.log(level, message as string, ...meta);
+  };
+}
+
+export const Log: Plugin.Container = {
+  name,
+  dependencies,
+  configure,
+  options,
+  init,
+  get,
+  log: logger.log.bind(logger),
+  trace: namedLogMethod('trace'),
+  debug: namedLogMethod('debug'),
+  info: namedLogMethod('info'),
+  warning: namedLogMethod('warning'),
+  error: namedLogMethod('error'),
+  fatal: namedLogMethod('fatal')
+};
+
+export { Log as default };
