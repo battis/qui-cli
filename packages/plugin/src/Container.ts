61b212214858f78fa5352f00f7b3abe78dc43b11
Seth Battis <seth@battis.net>
2025-02-10 07:04:52 -0500

    feat(qui-cli.plugin)!: nest _meta and _hooks for clearer end user types
    BREAKING: Plugin.Container module implementations
    will need to nest name and dependencies into the
    _meta property, and configure, options, and init into the
    _hooks property.

--- packages/plugin/src/Container.ts
+++ packages/plugin/src/Container.ts
@@ -4,20 +4,24 @@ import { Options } from './Options.js';
 export type Configuration = {};
 
 export type Container = { [key: string]: any } & {
-  name: string;
-  dependencies: string[];
+  _meta: {
+    name: string;
+    dependencies: string[];
+  };
 
-  configure: (config?: Configuration) => Promise<void>;
+  _hooks: {
+    configure: (config?: Configuration) => Promise<void>;
 
-  /**
-   * Override to include custom Jackspeak configuration.
-   *
-   * Will be called _before_ init() has been called.
-   */
-  options: () => Options;
+    /**
+     * Override to include custom Jackspeak configuration.
+     *
+     * Will be called _before_ init() has been called.
+     */
+    options: () => Options;
 
-  /**
-   * Override to process user-provided arguments parsed by Jackspeak
-   */
-  init: (args: Arguments<ReturnType<Container['options']>>) => void;
+    /**
+     * Override to process user-provided arguments parsed by Jackspeak
+     */
+    init: (args: Arguments<ReturnType<Container['options']>>) => void;
+  };
 };
