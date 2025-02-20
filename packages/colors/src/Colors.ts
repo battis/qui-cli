84a807bef81db54f12c259cbc0e89adce9e3c3dd
Seth Battis <seth@battis.net>
2025-02-19 22:28:07 -0500

    feat(qui-cli.colors)!: reimplement as ESM module
    Dramatically simplify code necessary to implement plugin.
    
    BREAKING: Incompatible with previous plugin architecture relying on
    extensions of TypeScript Plugin.Base class.

---
+++ packages/colors/src/Colors.ts
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+
+export const name = 'colors';
+export const src = import.meta.dirname;
+
+export const value = chalk.yellow;
+export const quotedValue = chalk.green;
+export const regexpValue = chalk.red;
+export const url = chalk.cyan;
+export const error = chalk.red.bold;
+export const command = chalk.magenta;
+export const keyword = chalk.magenta.bold;
--- packages/colors/src/index.ts
+++ packages/colors/src/index.ts
@@ -1,32 +1,5 @@
-import * as plugin from '@battis/qui-cli.plugin';
-import chalk from 'chalk';
+import { register } from '@battis/qui-cli.plugin';
+import * as Colors from './Colors.js';
 
-export class Colors extends plugin.Base {
-  private static singleton?: Colors;
-
-  public static getInstance() {
-    if (!this.singleton) {
-      this.singleton = new Colors();
-    }
-    return this.singleton;
-  }
-
-  public constructor() {
-    super('colors');
-    if (Colors.singleton) {
-      throw new Error('Colors is a singleton');
-    } else {
-      Colors.singleton = this;
-    }
-  }
-
-  public value = chalk.yellow;
-  public quotedValue = chalk.green;
-  public regexpValue = chalk.red;
-  public url = chalk.cyan;
-  public error = chalk.red.bold;
-  public command = chalk.magenta;
-  public keyword = chalk.magenta.bold;
-}
-
-export default Colors.getInstance();
+await register(Colors);
+export { Colors };
