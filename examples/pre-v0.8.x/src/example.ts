e5588543f40beccf39d686d4c8dee3ba61cdf708
Seth Battis <seth@battis.net>
2025-02-19 22:34:31 -0500

    docs: update example to reflect changed plugin architecture

--- examples/pre-v0.8.x/src/example.ts
+++ examples/pre-v0.8.x/src/example.ts
@@ -1,22 +1,18 @@
 import cli from '@battis/qui-cli';
 
-const {
-  positionals: [arg0, arg1],
-  values: { opt0, opt1, flag0, flag1, ...rest }
-} = cli.init({
-  args: {
-    allowPositionals: true,
-    options: {
-      opt0: {},
-      opt1: {}
-    },
-    flags: {
-      flag0: { default: false },
-      flag1: { default: false }
-    }
+cli.configure({ allowPositionals: true });
+
+const args = cli.init({
+  opt: {
+    opt0: {},
+    opt1: {}
+  },
+  flag: {
+    flag0: { default: false },
+    flag1: { default: false }
   }
 });
 
-cli.log.info({ flag0, flag1, opt0, opt1, arg0, arg1, rest });
+cli.log.info({ args });
 
-cli.shell.mv('r', 'src', 'dest');
+cli.shell.exec('echo "hello world"');
