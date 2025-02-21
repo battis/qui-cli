3c38c3aa1aaaf78415f4c48090045b3dff8f17a5
Seth Battis <seth@battis.net>
2025-02-20 22:30:05 -0500

    docs: label lightweight example

--- examples/lightweight/src/example.ts
+++ examples/lightweight/src/example.ts
@@ -1,5 +1,8 @@
 import { Core } from '@battis/qui-cli.core';
 import { Log } from '@battis/qui-cli.log';
 
+// process user command-line options
 Core.init({ opt: { foo: { description: 'foo bar' } } });
+
+// use plugin
 Log.info(import.meta.dirname);
