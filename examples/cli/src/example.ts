6bbbc04c2e059d8acc3c13c9eab2b1ad5b2fb0d6
Seth Battis <seth@battis.net>
2025-02-20 22:29:02 -0500

    docs: label examples in cli

--- examples/cli/src/example.ts
+++ examples/cli/src/example.ts
@@ -2,11 +2,13 @@ import cli from '@battis/qui-cli';
 import { input } from '@inquirer/prompts';
 import path from 'node:path';
 
+// configuration before command-line user options are processed
 cli.env.configure({ path: path.resolve(import.meta.dirname, '../.env') });
 cli.configure({
   allowPositionals: true
 });
 
+// process command-line user options
 const args = cli.init({
   opt: {
     opt0: {},
@@ -17,25 +19,29 @@ const args = cli.init({
     flag1: { default: false }
   }
 });
+cli.log.info({ args });
 
+// @battis/qui-cli.env example
 cli.env.set({
   key: 'RUNS',
   value: `${parseInt(cli.env.get({ key: 'RUNS' }) || '0') + 1}`
 });
 cli.log.info(`${process.env.RUNS} runs (.env updated)`);
 
-cli.log.info({ args });
-
+// @battis/qui-cli.log and @battis/qui-cli.colors examples
 cli.log.error(
   cli.colors.error(
     `A fake error with ${cli.colors.value('a value')} and a ${cli.colors.quotedValue(`"quoted value"`)}`
   )
 );
 
+// @battis/qui-cli.root example
 cli.log.info(cli.colors.url(cli.root.path()));
 
+// @battis/qui-cli.shell example
 cli.shell.exec('echo "hello world"');
 
+// @battis/qui-cli.validators example
 const text = await input({
   message: 'Enter a long-ish word',
   default: 'quetzlcoatl',
@@ -44,6 +50,8 @@ const text = await input({
     cli.validators.maxLength(100)
   )
 });
+
+// @battis/qui-cli.progress example
 cli.progress.start({ value: 0, max: text.length });
 for (let i = 0; i < text.length; i++) {
   setTimeout(() => {
