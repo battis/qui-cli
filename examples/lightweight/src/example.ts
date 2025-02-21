5a0af45c9b636b4a2de014bd4f7b4d2b9a2c6908
Seth Battis <seth@battis.net>
2025-02-20 22:31:01 -0500

    docs: tweak lightweight example to be less arbitrary

--- examples/lightweight/src/example.ts
+++ examples/lightweight/src/example.ts
@@ -2,7 +2,7 @@ import { Core } from '@battis/qui-cli.core';
 import { Log } from '@battis/qui-cli.log';
 
 // process user command-line options
-Core.init({ opt: { foo: { description: 'foo bar' } } });
+const args = Core.init({ opt: { foo: { description: 'foo bar' } } });
 
 // use plugin
-Log.info(import.meta.dirname);
+Log.info(args);
