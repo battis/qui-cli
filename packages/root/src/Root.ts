c774f6ba708ffc5a78f653df47de523834000d66
Seth Battis <seth@battis.net>
2025-02-27 10:08:11 -0500

    fix(qui-cli.root): actually default cwd to true

--- packages/root/src/Root.ts
+++ packages/root/src/Root.ts
@@ -7,11 +7,18 @@ export const name = 'root';
 export const src = import.meta.dirname;
 
 export type Configuration = Plugin.Configuration & {
+  /** base for resolving any relative file paths */
   root?: string;
+
+  /**
+   * use `root` as the current working directory (default `true`) or path to
+   * an alternate working directory (optionally relative to `root`)
+   */
   cwd?: string | boolean;
 };
 
 let root: string = appRoot.toString();
+let cwd: string | boolean = true;
 
 export function path() {
   return root;
@@ -19,11 +26,12 @@ export function path() {
 
 export function configure(config: Configuration = {}) {
   root = Plugin.hydrate(config.root, root);
-  if (config.cwd) {
-    if (config.cwd === true) {
+  cwd = Plugin.hydrate(config.cwd, cwd);
+  if (cwd) {
+    if (typeof cwd === 'boolean') {
       process.chdir(root);
     } else {
-      process.chdir(nodePath.resolve(root, config.cwd));
+      process.chdir(nodePath.resolve(root, cwd));
     }
   }
 }
