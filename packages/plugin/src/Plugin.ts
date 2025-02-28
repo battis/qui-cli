4fe3df0429c625ab4b2471c3d3bdd20ca38b25aa
Seth Battis <seth@battis.net>
2025-02-28 11:00:37 -0500

    refactor(qui-cli.plugin): remove type parameter on Plugin.Base

--- packages/plugin/src/Plugin.ts
+++ packages/plugin/src/Plugin.ts
@@ -2,7 +2,7 @@ import * as Configuration from './Configuration.js';
 import * as Initialization from './Initialization.js';
 import * as Options from './Options.js';
 
-export type Base<O extends Options.Options = Options.Options> = {
+export type Base = {
   name: string;
   src: string;
   configure?: Configuration.Hook;
