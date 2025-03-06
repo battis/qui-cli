f6110c1cc8f24cd4e2bec7eb749471c6bbbfa011
Seth Battis <seth@battis.net>
2025-03-06 09:46:23 -0500

    chore(plugin): issue tracking

--- packages/plugin/src/Initialization.ts
+++ packages/plugin/src/Initialization.ts
@@ -1,18 +1,16 @@
 import { ConfigSetFromMetaSet, OptionsResults } from 'jackspeak';
 import * as Options from './Options.js';
 
-type FlattenConfigMetaSets<O extends Options.Options> = ConfigSetFromMetaSet<
-  'number',
-  false,
-  Exclude<O['num'], undefined>
-> &
-  ConfigSetFromMetaSet<'number', true, Exclude<O['numList'], undefined>> &
-  ConfigSetFromMetaSet<'string', false, Exclude<O['opt'], undefined>> &
-  ConfigSetFromMetaSet<'string', false, Exclude<O['opt'], undefined>> &
-  ConfigSetFromMetaSet<'string', true, Exclude<O['optList'], undefined>> &
-  ConfigSetFromMetaSet<'boolean', false, Exclude<O['flag'], undefined>> &
-  ConfigSetFromMetaSet<'boolean', true, Exclude<O['flagList'], undefined>> &
-  Exclude<O['fields'], undefined>;
+type FlattenConfigMetaSets<O extends Options.Options> =
+  // FIXME all options are getting typed `string | undefined`
+  ConfigSetFromMetaSet<'number', false, Exclude<O['num'], undefined>> &
+    ConfigSetFromMetaSet<'number', true, Exclude<O['numList'], undefined>> &
+    ConfigSetFromMetaSet<'string', false, Exclude<O['opt'], undefined>> &
+    ConfigSetFromMetaSet<'string', false, Exclude<O['opt'], undefined>> &
+    ConfigSetFromMetaSet<'string', true, Exclude<O['optList'], undefined>> &
+    ConfigSetFromMetaSet<'boolean', false, Exclude<O['flag'], undefined>> &
+    ConfigSetFromMetaSet<'boolean', true, Exclude<O['flagList'], undefined>> &
+    Exclude<O['fields'], undefined>;
 
 export type Arguments<O extends Options.Options> = {
   positionals: (string | undefined)[];
