a1e4d91db4ac1a7a8fc6f9eb03d6c3aa57d0d8be
Seth Battis <seth@battis.net>
2025-06-22 21:01:12 -0400

    refactor: simplify reexports

--- packages/qui-cli/src/CLI.ts
+++ packages/qui-cli/src/CLI.ts
@@ -1,24 +1,13 @@
-import { Colors } from '@battis/qui-cli.colors';
-import { Core } from '@battis/qui-cli.core';
-import { Env } from '@battis/qui-cli.env';
-import { Log } from '@battis/qui-cli.log';
-import { Progress } from '@battis/qui-cli.progress';
-import { Root } from '@battis/qui-cli.root';
-import { Shell } from '@battis/qui-cli.shell';
-import { Validators } from '@battis/qui-cli.validators';
+import * as Help from '@battis/qui-cli.core/dist/Help.js';
+import { register } from '@battis/qui-cli.plugin';
 
-export const colors = Colors;
-export const env = Env;
-export const log = Log;
-export const progress = Progress;
-export const root = Root;
-export const shell = Shell;
-export const validators = Validators;
+await register(Help);
 
-export type Configuration = Core.Configuration;
-export type Options = Core.Options;
-
-export const configure = Core.configure;
-export const options = Core.options;
-export const init = Core.init;
-export const run = Core.run
+export { Colors as colors } from '@battis/qui-cli.colors';
+export * from '@battis/qui-cli.core/dist/Core.js';
+export { Env as env } from '@battis/qui-cli.env';
+export { Log as log } from '@battis/qui-cli.log';
+export { Progress as progress } from '@battis/qui-cli.progress';
+export { Root as root } from '@battis/qui-cli.root';
+export { Shell as shell } from '@battis/qui-cli.shell';
+export { Validators as validators } from '@battis/qui-cli.validators';
