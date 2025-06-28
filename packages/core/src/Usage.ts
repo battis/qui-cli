01fd5a2c6a08436a558f85343b7eec47556d1727
Seth Battis <seth@battis.net>
2025-06-27 21:42:14 -0400

    feat!: color-code command terms in -h usage output
    BREAKING: Also removed last available `usageMarkdown()`
    method. Use JackSpeak.jack().usageMarkdown() if needed.

--- packages/core/src/Usage.ts
+++ packages/core/src/Usage.ts
@@ -1,10 +1,18 @@
+import * as Colors from '@battis/qui-cli.colors/dist/Colors.js';
 import * as JackSpeak from './JackSpeak.js';
 import * as Positionals from './Positionals.js';
 
-export function usage() {
-  return Positionals.usage(JackSpeak.usage());
+function commandColor(usage: string) {
+  const pre = usage.slice(0, usage.indexOf('\n  ') + 3);
+  const cmd = usage.slice(pre.length, usage.indexOf(' ', pre.length));
+  const terms = usage.slice(
+    pre.length + cmd.length,
+    usage.indexOf('-', pre.length + cmd.length) - 1
+  );
+  const post = usage.slice(pre.length + cmd.length + terms.length);
+  return `${pre}${Colors.command(`${Colors.keyword(cmd)}${terms}`)}${post}`;
 }
 
-export function usageMarkdown() {
-  return Positionals.usageMarkdown(JackSpeak.usageMarkdown());
+export function usage() {
+  return Positionals.usage(commandColor(JackSpeak.usage()));
 }
