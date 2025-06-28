aea3ec45d27fcce20ea726e1a14477ff05c8005b
Seth Battis <seth@battis.net>
2025-06-27 21:27:28 -0400

    feat: more man page colors

--- packages/colors/src/Colors.ts
+++ packages/colors/src/Colors.ts
@@ -2,11 +2,18 @@ import chalk from 'chalk';
 
 export const name = 'colors';
 
+// console.log styles
 export const value = chalk.yellow;
 export const quotedValue = chalk.green;
 export const regexpValue = chalk.red;
-export const url = chalk.cyan;
+export const url = chalk.blue;
 export const error = chalk.red.bold;
+
+// node-stule
 export const command = chalk.magenta;
-export const keyword = chalk.magenta.bold;
+export const keyword = chalk.bold;
+
+// man page styles
+export const optionArg = value;
+export const flagArg = value;
 export const positionalArg = chalk.underline;
