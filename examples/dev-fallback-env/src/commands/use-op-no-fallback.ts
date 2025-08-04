a4bdea6bc3c038f0c26e3281c8648294d55d36b0
Seth Battis <seth@battis.net>
2025-08-04 14:04:35 -0400

    docs: highlight first import

--- examples/dev-fallback-env/src/commands/use-op-no-fallback.ts
+++ examples/dev-fallback-env/src/commands/use-op-no-fallback.ts
@@ -1,5 +1,7 @@
-import { Core } from '@qui-cli/core';
+// imported before MyPlugin (and separated to keep prettier from reordering it!)
 import '@qui-cli/env/1Password.js';
+
+import { Core } from '@qui-cli/core';
 import { register } from '@qui-cli/plugin';
 import { Root } from '@qui-cli/root';
 import path from 'node:path';
