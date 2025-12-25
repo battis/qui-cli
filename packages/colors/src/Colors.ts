e64a88e8a2c38fe485fa2527fe5c3953dce4b950
Seth Battis <seth@battis.net>
2025-12-25 09:54:31 -0500

    feat: add highlighting options for paths and commands

--- packages/colors/src/Colors.ts
+++ packages/colors/src/Colors.ts
@@ -1,30 +1,42 @@
-import chalk from 'chalk';
+import Chalk from 'chalk';
+import nodePath from 'node:path';
 
 // console.log styles
 /** Literal values that don't match other, more specific, categories */
-export const value = chalk.yellow;
+export const value = Chalk.yellow;
 /** Variable and constant names */
 export const varName = value;
 /**
  * A quoted value (e.g. `"hello world"`). Quotation marks should be included in
  * the `text` argument.
  */
-export const quotedValue = chalk.green;
+export const quotedValue = Chalk.green;
 /**
  * A regular expression value (e/g `/.+/`). Expression delimiters (`/`) should
  * be included in the `text` argument.
  */
-export const regexpValue = chalk.red;
+export const regexpValue = Chalk.red;
 /** A URL value (e.g. `https://example.com` or `./path/to/file`). */
-export const url = chalk.blue;
+export const url = Chalk.blue;
+
 /** A file or URL path (e.g. `../../path/to/file`). */
-export const path = url;
+export function path(value: string, highlight = (v: string) => v) {
+  return url(
+    `${nodePath.dirname(value)}${value.indexOf('/') >= 0 ? '/' : ''}${highlight(nodePath.basename(value))}`
+  );
+}
+
 /** An error message (e.g. `Bad things happened!`). */
-export const error = chalk.red.bold;
+export const error = Chalk.red.bold;
 
 // node-style
 /** A shell command (e.g. `echo "hello world"`). */
-export const command = chalk.magenta;
+export function command(value: string, highlight = (v: string) => v) {
+  const tokens = value.split(' ');
+  return Chalk.magenta(
+    `${highlight(tokens.shift() || '')} ${tokens.join(' ')}`
+  );
+}
 /**
  * A keyword (e.g. a shell command), as in:
  *
@@ -32,7 +44,7 @@ export const command = chalk.magenta;
  * console.log(Colors.command(`${Colors.keyword('echo')} "hello world"`));
  * ```
  */
-export const keyword = chalk.bold;
+export const keyword = Chalk.bold;
 
 // man page styles
 /**
@@ -48,4 +60,4 @@ export const optionArg = value;
 /** The name of a flag argument (e.g. `--no-errors-please`). */
 export const flagArg = value;
 /** The name of a positional argument (e.g. `arg0`). */
-export const positionalArg = chalk.underline;
+export const positionalArg = Chalk.underline;
