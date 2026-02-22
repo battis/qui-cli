5828d894a37950d89f16003bf2b815543cfa9113
Seth Battis <seth@battis.net>
2026-02-22 14:08:01 -0500

    feat: convert ANSI coloring to markdown

--- examples/dev-readme/README.md
+++ examples/dev-readme/README.md
@@ -11,7 +11,7 @@ Demonstrate using `@qui-cli/markdown` to generate a README
 ## Usage:
 
 ```bash
-  dev-readme -hA --z=<buzz> --bargle --commands --silent --logging --baz=<baz> --biz=<biz> --pi=<pi> --a=<a> --opAccount=<example.1password.com> --opItem=<1Password unique identifier> --opToken=<token value> --logFilePath=<logFilePath> --stdoutLevel=<all|trace|debug|info|warning|error|fatal|off> --fileLevel=<all|trace|debug|info|warning|error|fatal|off> foo bar
+  dev-readme -hA --z=<buzz> --bargle --commands --silent --logging --baz=<baz> --biz=<biz> --pi=<pi> --a=<a> --opAccount=<example.1password.com> --opItem=<1Password unique identifier> --opToken=<token value> --logFilePath=<logFilePath> --stdoutLevel=<all|trace|debug|info|warning|error|fatal|off> --fileLevel=<all|trace|debug|info|warning|error|fatal|off> `foo` `bar`
 ```
 
 ## Arguments
@@ -22,11 +22,11 @@ Show this usage information
 
 ### Positional arguments
 
-#### foo
+#### `foo`
 
 Enim minim laborum dolore, eiusmod.
 
-#### bar
+#### `bar`
 
 Cillum ut sit, labore.
 
@@ -38,7 +38,7 @@ Sint ipsum aliquip veniam tempor occaecat.
 
 #### `--bargle`
 
-Tempor minim anim exercitation aliquip. (Default: true, use --no-bargle to disable)
+Tempor minim anim exercitation aliquip. (Default: `true`, use `--no-bargle` to disable)
 
 #### `--baz=<baz>`
 
@@ -46,17 +46,17 @@ Reprehenderit consequat, veniam velit.
 
 #### `-z<buzz> --buzz=<buzz>`
 
-Aliquip sunt tempor nisi labore ad culpa. (Default: "Sit")
+Aliquip sunt tempor nisi labore ad culpa. (Default: `"Sit"`)
 
 #### `--biz=<biz>`
 
 #### `--pi=<n>`
 
-Esse eiusmod exercitation nulla nostrud do velit id. (Default: 3.2)
+Esse eiusmod exercitation nulla nostrud do velit id. (Default: `3.2`)
 
 #### `--a=<a>`
 
-Default: "foo", "bar", "baz" Can be set multiple times
+Default: `"foo"`, `"bar"`, `"baz"` Can be set multiple times
 
 ### 1Password environment integration
 
@@ -64,29 +64,29 @@ If 1Password secret references are stored in the environment, a 1Password servic
 
 #### `--opAccount=<example.1password.com>`
 
-1Password account to use (if signed into multiple); will use environment variable OP_ACCOUNT if present
+1Password account to use (if signed into multiple); will use environment variable `OP_ACCOUNT` if present
 
 #### `--opItem=<1Password unique identifier>`
 
-Name or ID of the 1Password API Credential item storing the 1Password service account token; will use environment variable OP_ITEM if present. Requires the 1Password CLI tool (https://developer.1password.com/docs/cli)
+Name or ID of the 1Password API Credential item storing the 1Password service account token; will use environment variable `OP_ITEM` if present. Requires the 1Password CLI tool (`https://developer.1password.com/docs/cli`)
 
 #### `--opToken=<token value>`
 
-1Password service account token; will use environment variable OP_TOKEN if present
+1Password service account token; will use environment variable `OP_TOKEN` if present
 
 ### Shell command options
 
 #### `--commands`
 
-Include shell commands in log (Default: true, use --no-commands to disable)
+Include shell commands in log (Default: `true`, use `--no-commands` to disable)
 
 #### `--silent`
 
-Hide command output (Default: false)
+Hide command output (Default: `false`)
 
 #### `--logging`
 
-Log commands and output at level debug (Default: true, use --no-logging to disable)
+Log commands and output at level `debug` (Default: `true`, use `--no-logging` to disable)
 
 ### Logging options
 
@@ -96,8 +96,8 @@ Path to log file (optional)
 
 #### `--stdoutLevel=<all|trace|debug|info|warning|error|fatal|off>`
 
-Log level to console stdout (Default: "info")
+Log level to console stdout (Default: `"info"`)
 
 #### `--fileLevel=<all|trace|debug|info|warning|error|fatal|off>`
 
-Log level to log file if --logFilePath provided (Default: "all")
+Log level to log file if `--logFilePath` provided (Default: `"all"`)
--- packages/markdown/src/Markdown.ts
+++ packages/markdown/src/Markdown.ts
@@ -14,70 +14,121 @@ export type Configuration = Plugin.Configuration & {
    * [`fileName`](#fileName).
    */
   outputPath?: string;
+
   /**
    * Filename to use for output if `outputPath` is a path to a directory.
    * Default: `"usage.md"`.
    */
   fileName?: string;
+
   /** Any Markdown text to prepend to the output. */
   pre?: string;
+
   /** Amount to increment (or decrement) heading levels in the Markdown output. */
   headingLevelAdjustment?: number;
+
   /** Any Markdown text to append to the output. */
   post?: string;
+
   /** Whether or not to overwrite an existing file with output. Default: `false` */
   overwrite?: boolean;
+
+  /** Style configuration options */
+  style?: Partial<Record<keyof typeof Colors, [string, string]>>;
 };
 
 export const name = 'markdown';
 
-let outputPath = '.';
-let fileName = 'usage.md';
-let pre = '';
-let headingLevelAdjustment = 0;
-let post = '';
-let overwrite = false;
+const config: Configuration & {
+  outputPath: string;
+  fileName: string;
+  pre: string;
+  post: string;
+  headingLevelAdjustment: number;
+  style: NonNullable<Configuration['style']>;
+} = {
+  outputPath: '.',
+  fileName: 'usage.md',
+  pre: '',
+  headingLevelAdjustment: 0,
+  post: '',
+  overwrite: false,
+  style: {
+    value: ['`', '`'],
+    keyword: ['*', '*']
+  }
+};
+const placeholder = '@';
 
-export function configure(config: Configuration = {}) {
-  outputPath = Plugin.hydrate(config.outputPath, outputPath);
-  fileName = Plugin.hydrate(config.fileName, fileName);
-  pre = Plugin.hydrate(config.pre, pre);
-  headingLevelAdjustment = Plugin.hydrate(
-    config.headingLevelAdjustment,
-    headingLevelAdjustment
-  );
-  post = Plugin.hydrate(config.post, post);
-  overwrite = Plugin.hydrate(config.overwrite, overwrite);
+export function restyle(text: string) {
+  for (const key of Object.keys(Colors) as (keyof typeof Colors)[]) {
+    const replacement = config.style[key] || config.style.value;
+    if (replacement) {
+      const [ansiOpen, ansiClose] = Colors[key](placeholder).split(placeholder);
+      const [markdownOpen, markdownClose] = replacement;
+      let start;
+      while ((start = text.indexOf(ansiOpen)) >= 0) {
+        const end = text.indexOf(ansiClose, start);
+        text =
+          text.slice(0, start) +
+          markdownOpen +
+          text.slice(start + ansiOpen.length, end) +
+          markdownClose +
+          text.slice(end + ansiClose.length);
+      }
+    }
+  }
+  return text;
+}
+
+export function configure(proposal: Configuration = {}) {
+  for (const key in proposal) {
+    if (proposal[key] !== undefined) {
+      switch (key) {
+        case 'style':
+          for (const k of Object.keys(
+            proposal.style || {}
+          ) as (keyof Configuration['style'])[]) {
+            if (proposal.style && proposal.style[k] !== undefined) {
+              config.style[k] = proposal.style[k];
+            }
+          }
+          break;
+        default:
+          config[key] = proposal[key];
+      }
+    }
+  }
 }
 
 export async function run() {
-  outputPath = path.resolve(Root.path(), outputPath);
-  if (fs.existsSync(outputPath)) {
-    if (fs.statSync(outputPath).isFile()) {
-      if (!overwrite) {
+  config.outputPath = path.resolve(Root.path(), config.outputPath);
+  if (fs.existsSync(config.outputPath)) {
+    if (fs.statSync(config.outputPath).isFile()) {
+      if (!config.overwrite) {
         throw new Error(
-          `File ${Colors.url(outputPath)} already exists and will not be overwritten`
+          `File ${Colors.path(config.outputPath)} already exists and will not be overwritten`
         );
       }
     } else {
-      outputPath = path.join(outputPath, fileName);
+      config.outputPath = path.join(config.outputPath, config.fileName);
     }
   } else {
-    if (path.extname(outputPath) === '') {
-      outputPath = path.join(outputPath, fileName);
+    if (path.extname(config.outputPath) === '') {
+      config.outputPath = path.join(config.outputPath, config.fileName);
     }
-    fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+    fs.mkdirSync(path.dirname(config.outputPath), { recursive: true });
   }
   fs.writeFileSync(
-    outputPath,
-    `${pre.length ? `${pre}\n` : ''}${adjustHeadingLevel(
-      stripAnsi(Core.usageMarkdown())
+    config.outputPath,
+    `${config.pre.length ? `${config.pre}\n` : ''}${adjustHeadingLevel(
+      stripAnsi(restyle(Core.usageMarkdown()))
     )
       .replace('Usage:\n\n```', '## Usage:\n\n```bash')
       .replace(
         '#### `-h --help',
         '## Arguments\n\n#### `-h --help'
-      )}${post.length ? `\n${post}` : ''}`
+      )}${config.post.length ? `\n${config.post}` : ''}`
   );
 }
 
@@ -89,7 +140,7 @@ function adjustHeadingLevel(usage: string) {
       if (heading) {
         return line.replace(
           /^(#+)/,
-          '#'.repeat(heading.length + headingLevelAdjustment)
+          '#'.repeat(heading.length + config.headingLevelAdjustment)
         );
       }
       return line;
