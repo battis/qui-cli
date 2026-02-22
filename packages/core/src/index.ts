03f023a26df9563ba8e290a6da5d1d8eabcb725d
Seth Battis <seth@battis.net>
2026-02-22 09:07:05 -0500

    refactor: reorder to reflect current coding standards

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,9 +3,9 @@ import * as Help from './Help.js';
 import * as JackSpeak from './JackSpeak.js';
 import * as Positionals from './Positionals.js';
 
+export * as Core from './Core.js';
+export { Help, JackSpeak, Positionals };
+
 await register(Help);
 await register(JackSpeak);
 await register(Positionals);
-
-export * as Core from './Core.js';
-export { Help, JackSpeak, Positionals };
