278152ef017d8edc8e0048a1838533697bc98953
Seth Battis <seth@battis.net>
2026-07-02 10:18:13 -0400

    build: auto-generate examples TOC from package files

--- examples/README.md
+++ examples/README.md
@@ -1,17 +1,14 @@
-# Examples
+# examples
 
-## Usage Examples
-
-- [usage-cli](./usage-cli/): Using the bundled [@qui-cli/qui-cli](../packages/qui-cli/) package
-- [usage-modular](./usage-modular/): More parsimonious use of individual plugin modules independently
-
-## Development Examples
-
-- [dev-command](./dev-command/): A distributable command-line app
-- [dev-env-1password](./dev-env-1password): 1Password implementation of `@qui-cli/env` use
-- [dev-plugin](./dev-plugin/): Define (and use) a plugin package
-- [dev-plugin-lifecycle](./dev-plugin-lifecycle/): Demonstrate order in which plugin hooks are called (and the state of environment variables at each hook invocation).
-- [dev-positionals](./dev-positionals/): Require named or unnamed positional arguments
-- [dev-sequenced-commands](./dev-sequenced-commands/): A hierarchy of interdependent plugins
-- [dev-structured](./dev-structured/): A folder hierarchy of commands bundled into a single callable command
-- [dev-fallback-env](./dev-fallback-env/): Fallback to default Env implementation if OP is not imported
+<dl>
+<dt><a href="dev-command/README.md">@examples/dev-command</a></dt><dd>A distributable command-line app</dd>
+<dt><a href="dev-default-hooks/README.md">@examples/dev-default-hooks</a></dt><dd>Use the default hooks provided by @qui-cli/plugin</dd>
+<dt><a href="dev-env-1password/README.md">@examples/dev-env-1password</a></dt><dd>Integrate 1Password secret references in the enviroment with @qui-cli/env</dd>
+<dt><a href="dev-plugin/README.md">@examples/dev-plugin</a></dt><dd>Define (and use) a plugin package</dd>
+<dt><a href="dev-plugin-lifecycle/README.md">@examples/dev-plugin-lifecycle</a></dt><dd>Demonstrate order in which plugin hooks are called (and the state of environment variables at each hook invocation)</dd>
+<dt><a href="dev-positionals/README.md">@examples/dev-positionals</a></dt><dd>Require named or unnamed positional arguments</dd>
+<dt><a href="dev-sequenced-commands/README.md">@examples/dev-sequenced-commands</a></dt><dd>A hierarchy of interdependent plugins</dd>
+<dt><a href="dev-structured/README.md">@examples/dev-structured</a></dt><dd>A folder hierarchy of commands bundled into a single callable command</dd>
+<dt><a href="usage-cli/README.md">@examples/usage-cli</a></dt><dd>Using the bundled [@qui-cli/qui-cli](../packages/qui-cli/) package</dd>
+<dt><a href="usage-modular/README.md">@examples/usage-modular</a></dt><dd>More parsimonious use of individual plugin modules independently</dd>
+</dl>
---
+++ src/toc.ts
@@ -0,0 +1,182 @@
+import * as Plugin from '@qui-cli/plugin';
+import { Core, Positionals } from '@qui-cli/core';
+import fs from 'node:fs';
+import path from 'node:path';
+import { PathString } from '@battis/descriptive-types';
+import { Colors } from '@qui-cli/colors';
+import { Log } from '@qui-cli/log';
+import { IPackageJson } from 'package-json-type';
+
+type Configuration = Plugin.Configuration & {
+  scanPath?: PathString;
+  outputPath?: PathString;
+  overwrite?: boolean;
+  recursive?: boolean;
+  depth?: number;
+  templatePath?: PathString;
+};
+
+type Entry = {
+  name: string;
+  description: string;
+  readme: PathString;
+  subentries?: Entry[];
+};
+
+Positionals.require({
+  scanPath: { description: 'Path to a directory to scan for packages' }
+});
+Positionals.allowOnlyNamedArgs();
+
+const config: Configuration = {
+  overwrite: false,
+  recursive: false
+};
+
+function configure(proposal: Configuration = {}) {
+  for (const key in proposal) {
+    if (proposal[key] !== undefined) {
+      config[key] = proposal[key];
+    }
+  }
+}
+
+function options() {
+  return {
+    man: [{ level: 1, text: 'TOC Options' }],
+    opt: {
+      outputPath: {
+        description: `Path to TOC output file (defaults to ${Colors.path(`${Colors.positionalArg('scanPath')}/README.md`)})`,
+        short: 'o',
+        default: config.outputPath
+      },
+      templatePath: {
+        description: `Path to template into which to insert TOC at ${Colors.value(`{{TOC}}`)}`,
+        default: config.templatePath
+      }
+    },
+    flag: {
+      overwrite: {
+        description: 'Overwrite any existing TOC if present',
+        short: 'O',
+        default: config.overwrite
+      },
+      recursive: {
+        description: `Whether or not to recursively traverse ${Colors.positionalArg('scanPath')}`,
+        short: 'r',
+        default: config.recursive
+      }
+    },
+    num: {
+      depth: {
+        description: `If making at ${Colors.flagArg('--recursive')} scan, the maximum depth to scan`,
+        short: 'd',
+        default: config.depth
+      }
+    }
+  };
+}
+
+function init({ values }: Plugin.ExpectedArguments<typeof options>) {
+  configure({ scanPath: Positionals.get('scanPath'), ...values });
+}
+
+function run() {
+  if (!config.scanPath) {
+    Log.error(`${Colors.positionalArg('scanPath')} must be defined`);
+    process.exit(1);
+  }
+
+  config.scanPath = path.resolve(process.cwd(), config.scanPath);
+  if (!config.outputPath) {
+    config.outputPath = path.join(config.scanPath, 'README.md');
+  } else {
+    config.outputPath = path.resolve(process.cwd(), config.outputPath);
+  }
+
+  const entries = scan(config.scanPath);
+  if (entries) {
+    let toc = `# ${path.basename(config.scanPath)}\n\n${render(entries).join('\n')}`;
+    if (config.templatePath) {
+      config.templatePath = path.resolve(process.cwd(), config.templatePath);
+      if (fs.existsSync(config.templatePath)) {
+        toc = fs
+          .readFileSync(config.templatePath, 'utf8')
+          .replace('{{TOC}}', toc);
+      }
+    }
+    if (fs.existsSync(config.outputPath)) {
+      if (!config.overwrite) {
+        Log.error(
+          `Cannot overwrite existing ${Colors.path(config.outputPath)} (set ${Colors.flagArg('--overwrite')})`
+        );
+        Log.info(toc);
+        process.exit(2);
+      }
+      Log.warning(
+        `Overwriting existing ${Colors.path(path.basename(config.outputPath))}`
+      );
+    }
+    fs.writeFileSync(config.outputPath, toc);
+    Log.info(`TOC written to ${Colors.path(config.outputPath)}`);
+  } else {
+    Log.error(
+      `No indexable packages and README files found in ${Colors.path(config.scanPath)}`
+    );
+    process.exit(3);
+  }
+}
+
+function scan(scanPath: PathString, depth = 0) {
+  const toc: Entry[] = [];
+  for (const filename of fs.readdirSync(scanPath)) {
+    const entryPath = path.join(scanPath, filename);
+    if (
+      !['.', '..', 'node_modules'].includes(filename) &&
+      fs.statSync(entryPath).isDirectory()
+    ) {
+      const packagePath = path.join(entryPath, 'package.json');
+      if (fs.existsSync(packagePath)) {
+        const pkg: IPackageJson = JSON.parse(
+          fs.readFileSync(packagePath, 'utf8')
+        );
+        const readme = path.join(entryPath, 'README.md');
+        if (pkg.name && pkg.description && fs.existsSync(readme)) {
+          toc.push({
+            name: pkg.name,
+            description: pkg.description,
+            readme,
+            subentries:
+              config.recursive && (!config.depth || depth < config.depth)
+                ? scan(entryPath, depth + 1)
+                : undefined
+          });
+        }
+      }
+    }
+  }
+  if (toc.length) {
+    return toc;
+  }
+  return undefined;
+}
+
+function render(entries: Entry[], depth = 0) {
+  const lines: string[] = ['<dl>'];
+  for (const entry of entries) {
+    lines.push(
+      `<dt><a href="${path.relative(config.scanPath || config.scanPath || process.cwd(), entry.readme)}">${entry.name}</a></dt><dd>${entry.description}</dd>`
+    );
+    if (entry.subentries) {
+      lines.push(...render(entry.subentries, depth + 1));
+    }
+  }
+  if (entries.length) {
+    lines.push('</dl>');
+    return lines;
+  }
+  return [];
+}
+
+await Plugin.register({ name: 'toc', configure, options, init, run });
+await Core.run();
