d8155cef6a4cf5fd838540ef4996b4a4c622ad04
Seth Battis <seth@battis.net>
2026-07-04 18:11:46 -0400

    chore: move pkg-setup to @qui-cli

---
+++ packages/pkg-setup/src/Setup/index.ts
@@ -0,0 +1,6 @@
+export * from './Plugin.js';
+export * as confirm from './confirm/index.js';
+export * from './isEqual.js';
+export * from './mergeJSONValues.js';
+export * as FileHandlers from './FileHandlers/index.js';
+export * from './Configuration.js';
