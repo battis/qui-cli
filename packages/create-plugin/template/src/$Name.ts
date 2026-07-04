f03d43372a2862cf74e4592e80f44dced05bba66
Seth Battis <seth@battis.net>
2026-07-04 16:55:36 -0400

    feat: create-plugin template

---
+++ packages/create-plugin/template/src/$Name.ts
@@ -0,0 +1,7 @@
+import * as Plugin from '@qui-cli/plugin';
+
+export type Configuration = Plugin.Configuration & {};
+
+export const name = '$NAME';
+
+export async function run() {}
---
+++ packages/create-plugin/template/src/index.ts
@@ -0,0 +1,5 @@
+import { register } from '@qui-cli/plugin';
+import * as $Name from './$Name.js';
+
+await register($Name);
+export { $Name };
