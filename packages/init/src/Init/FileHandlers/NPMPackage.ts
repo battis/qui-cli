086e2e7378d70229e17b91b6c721515097144bc7
Seth Battis <seth@battis.net>
2026-07-07 17:39:01 -0400

    fix: infer package name, remove flaky spinner

--- packages/init/src/Init/FileHandlers/NPMPackage.ts
+++ packages/init/src/Init/FileHandlers/NPMPackage.ts
@@ -8,9 +8,12 @@ import { IPackageJson } from 'package-json-type';
 import * as Placeholders from '../../Placeholders.js';
 import * as Plugin from '@qui-cli/plugin';
 import prettier from 'prettier';
+import { PathString } from '@battis/descriptive-types';
+import { Log } from '@qui-cli/log';
 import { kebabCase } from 'change-case';
 
 export type Configuration = Plugin.Configuration & {
+  packagePath?: PathString;
   /** Name (without scope) for the project being initialized */
   name?: string;
   /** Scope for the project being initialized */
@@ -24,11 +27,46 @@ const config: Configuration = {
 };
 
 export function configure(proposal: Configuration = {}) {
+  // process command line options
   for (const key in proposal) {
     if (proposal[key] !== undefined) {
       config[key] = proposal[key];
     }
   }
+
+  // try to read unset package name and scope from local package.json
+  if (
+    config.packagePath &&
+    !config.name &&
+    fs.existsSync(path.resolve(process.cwd(), config.packagePath))
+  ) {
+    const pkg: IPackageJson = JSON.parse(
+      fs.readFileSync(path.resolve(process.cwd(), config.packagePath), 'utf8')
+    );
+    const [, , scope, name] = pkg.name?.match(/^(@([^/]+)\/)?(.+)$/) || [];
+    config.scope = scope || config.scope;
+    config.name = name || config.name;
+  }
+
+  // infer unset package name from path to not-yet-created package.json
+  if (!config.name && config.packagePath) {
+    config.name = path.basename(path.dirname(config.packagePath));
+  }
+
+  const placeholders: Placeholders.Configuration['placeholders'] = {
+    description: [config.description || '']
+  };
+  if (config.scope) {
+    placeholders.scope = [config.scope];
+  }
+  if (config.name) {
+    placeholders.name = [
+      config.name,
+      `${config.scope ? `@${kebabCase(config.scope)}/` : ''}${kebabCase(config.name)}`,
+      `${config.scope ? `@${kebabCase(config.scope)}%2f` : ''}${kebabCase(config.name)}`
+    ];
+  }
+  Placeholders.configure({ placeholders });
 }
 
 export function options() {
@@ -50,18 +88,6 @@ export function options() {
 
 export function init({ values }: Plugin.ExpectedArguments<typeof options>) {
   configure(values);
-  if (config.name) {
-    Placeholders.configure({
-      placeholders: {
-        name: [
-          config.name,
-          config.scope
-            ? `@${kebabCase(config.scope)}/${kebabCase(config.name)}`
-            : kebabCase(config.name)
-        ]
-      }
-    });
-  }
 }
 
 export const handle: FileHandler['handle'] = async ({
@@ -69,38 +95,51 @@ export const handle: FileHandler['handle'] = async ({
   destPath,
   force = false
 }) => {
-  const pkg: IPackageJson = fs.existsSync(destPath)
-    ? JSON.parse(fs.readFileSync(destPath, 'utf8'))
-    : {};
   const proposal = JSON.parse(
     Placeholders.replaceAll(fs.readFileSync(srcPath, 'utf8'))
   );
   if (config.private) {
     proposal.private = true;
   }
-  let changed = false;
-  for (const key in proposal) {
-    const update = merge(proposal[key], pkg[key]);
-    await Confirm.withDiff({
-      src: update,
-      dest: pkg[key],
-      identifier: Colors.value(`package.${key}`),
-      action: () => {
-        pkg[key] = update;
-        changed = true;
-      },
-      force
-    });
-  }
-  if (changed) {
+  if (fs.existsSync(destPath)) {
+    const pkg: IPackageJson = JSON.parse(fs.readFileSync(destPath, 'utf8'));
+    let changed = false;
+    for (const key in proposal) {
+      const update = merge(proposal[key], pkg[key]);
+      await Confirm.withDiff({
+        src: update,
+        dest: pkg[key],
+        identifier: Colors.value(`package.${key}`),
+        action: () => {
+          pkg[key] = update;
+          changed = true;
+        },
+        force
+      });
+    }
+    if (changed) {
+      fs.writeFileSync(
+        destPath,
+        await prettier.format(JSON.stringify(pkg), {
+          ...(await prettier.resolveConfig(import.meta.dirname)),
+          filepath: destPath
+        })
+      );
+      Log.info(`${Colors.path(destPath, Colors.keyword)} updated`);
+      return `Changes have been made to ${Colors.path(path.join(process.cwd(), 'package.json'), Colors.keyword)}, please verify package dependency and build status`;
+    } else {
+      Log.info(`${Colors.path(destPath, Colors.keyword)} up-to-date`);
+    }
+  } else {
     fs.writeFileSync(
       destPath,
-      await prettier.format(JSON.stringify(pkg), {
+      await prettier.format(JSON.stringify(proposal), {
         ...(await prettier.resolveConfig(import.meta.dirname)),
         filepath: destPath
       })
     );
-    return `Changes have been made to ${Colors.path(path.join(process.cwd(), 'package.json'), Colors.keyword)}, please verify package dependency and build status`;
+    Log.info(`${Colors.path(destPath, Colors.keyword)} created`);
+    return `A new ${Colors.path(path.join(process.cwd(), 'package.json'), Colors.keyword)} has been created, please install dependencies`;
   }
 };
 
