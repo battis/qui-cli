167392eca9ebb2ff5623278850059a6207e61c0f
Seth Battis <seth@battis.net>
2026-08-01 18:25:43 -0400

    fix: deprecate @qui-cli/validators

--- packages/qui-cli/src/CLI.ts
+++ packages/qui-cli/src/CLI.ts
@@ -7,4 +7,9 @@ export { Log as log } from '@qui-cli/log';
 export { Progress as progress } from '@qui-cli/progress';
 export { Root as root } from '@qui-cli/root';
 export { Shell as shell } from '@qui-cli/shell';
+
+/**
+ * @deprecated Use
+ *   {@link https://github.com/battis/qui-cli/tree/main/packages/validators#readme zod}
+ */
 export { Validators as validators } from '@qui-cli/validators';
